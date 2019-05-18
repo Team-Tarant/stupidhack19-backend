331da4a9a1a9c3afd9af41f100eb3935722f7d33
Hugo Holmqvist <hugo.holm@live.com>
2019-05-18 09:54:39 +0300

    fix style

--- src/services/contentService.js
+++ src/services/contentService.js
@@ -5,7 +5,7 @@ const { CONTENT_SERVICE_URL } = process.env
 const getContent = () => axios
   .get(CONTENT_SERVICE_URL)
   .then(({ data }) => data)
-  .then(({text}) => text)
+  .then(({ text }) => text)
   .catch(e => e.message)
 
 module.exports = {
