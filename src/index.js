f03fde332b6cd6f3c37e723b361be5d88eb91280
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-05-18 09:32:38 +0300

    Store calls in db so we can track responses

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const twilio = require('twilio')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Joi = require('@hapi/joi')
 const app = require('express')()
+const { Invitation } = require('./models')
 const { HttpErrors, PhoneNumberParseError } = require('./errors')
 const { parseNumbers, parseNumber } = require('./util')
 
@@ -14,8 +16,11 @@ const {
   TWILIO_NUMBER,
   TWIML_API_URL,
   PORT,
-  SUPER_API_KEY
+  SUPER_API_KEY,
+  SUPER_TWILIO_API_KEY,
+  MONGODB_URI
 } = process.env
+
 const client = twilio(TWILIO_SID, TWILIO_TOKEN)
 
 const qs = params =>
@@ -28,11 +33,10 @@ const makeUrl = {
     `${TWIML_API_URL}/twiml/come-bontho?${qs({ name, place })}`
 }
 
-const apiAuth = (req, res, next) => {
-  if (!req.query.api_key || req.query.api_key !== SUPER_API_KEY) {
+const apiAuth = expectedApiKey => (req, res, next) => {
+  if (!req.query.api_key || req.query.api_key !== expectedApiKey) {
     return next(HttpErrors.unauthorized('invalid api_key'))
   }
-
   next()
 }
 
@@ -40,7 +44,7 @@ app.use(bodyParser.json())
 
 /**
  * @param {string} number
- * @returns {Promise<any>}
+ * @returns {Promise<{sid: string} | {error: any}>}
  */
 const enqueueCall = (number, url) =>
   client.calls
@@ -72,7 +76,7 @@ const inviteSchema = Joi.object().keys({
     .required()
 })
 
-const validate = schema => (req, res, next) => {
+const validateBody = schema => (req, res, next) => {
   const { error, value } = Joi.validate(req.body, schema)
   if (error) {
     const msg = error.details.map(e => e.message).join(', ')
@@ -83,10 +87,44 @@ const validate = schema => (req, res, next) => {
   next()
 }
 
+app.get('/api/invitations', apiAuth(SUPER_API_KEY), async (req, res, next) => {
+  const invitations = await Invitation.find({})
+  res.json(invitations)
+})
+
+const responseSchema = Joi.object().keys({
+  sid: Joi.string().required(),
+  isDown: Joi.bool().required()
+})
+
+app.post(
+  '/api/response',
+  apiAuth(SUPER_TWILIO_API_KEY),
+  validateBody(responseSchema),
+  async (req, res) => {
+    const { sid, isDown } = req.locals.body
+    const invitation = await Invitation.findOne({ sid })
+
+    if (!invitation) {
+      // uhhh okay
+      return res.status(200).json({ ok: 'ok' })
+    }
+
+    if (invitation.hasResponded()) {
+      // uhhhhhhhh duplicate?
+      return res.status(200).json({ ok: 'ok' })
+    }
+
+    invitation.setIsDown(isDown)
+    await invitation.save()
+    res.status(200).json({ ok: 'ok' })
+  }
+)
+
 app.post(
   '/api/invite-bontho',
-  apiAuth,
-  validate(inviteSchema),
+  apiAuth(SUPER_API_KEY),
+  validateBody(inviteSchema),
   async (req, res, next) => {
     const { numbers, inviter, place } = req.locals.body
 
@@ -121,6 +159,13 @@ app.post(
       .filter(({ error }) => !!error)
       .map(data => ({ ...data, error: data.error.message }))
 
+    try {
+      // save queued calls to db
+      await Invitation.createSent(queued)
+    } catch (e) {
+      console.error('error while saving invitations to db', e)
+    }
+
     res.json({ queued, failed })
   }
 )
@@ -136,10 +181,22 @@ app.use((e, req, res, next) => {
   return res.status(500).json({ error: 'internal server error' })
 })
 
-app.listen(parseInt(PORT, 10), e => {
-  if (e) {
+console.log('Connecting to MongoDB')
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }).then(
+  () => {
+    console.log('Connected to MongoDB')
+    app.listen(parseInt(PORT, 10), e => {
+      if (e) {
+        console.error(`Failed to bind to port ${PORT}`)
+        console.error(e)
+        process.exit(1)
+      }
+      console.log(`Server listening on :${PORT}`)
+    })
+  },
+  e => {
+    console.error('Failed to connect to MongoDB', { MONGODB_URI })
     console.error(e)
     process.exit(1)
   }
-  console.log(`Server listening on :${PORT}`)
-})
+)
---
+++ src/models.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { parseNumber } = require('./util')
+
+const PHONE_BCRYPT_COST = 10
+
+const Responses = {
+  NO_RESPONSE_YET: null,
+  IS_DOWN: 'IS_DOWN',
+  IS_NOT_DOWN: 'IS_NOT_DOWN'
+}
+
+const invitationSchema = new mongoose.Schema(
+  {
+    phoneHash: String,
+    sid: String,
+    response: String
+  },
+  {
+    timestamps: true
+  }
+)
+
+/**
+ * @param {boolean} isDown
+ */
+invitationSchema.methods.setIsDown = function setIsDown(isDown) {
+  this.response = isDown ? Responses.IS_DOWN : Responses.IS_NOT_DOWN
+}
+
+invitationSchema.methods.hasResponded = function hasResponded() {
+  return this.response !== null
+}
+
+/**
+ * @param {{number: string, sid: string}[]} calls
+ */
+invitationSchema.statics.createSent = async function createSent(calls) {
+  // store the E.164 formatted number so that "040 123 4567" and "040 1234 567"
+  // are not different numbers
+  const callsToCreate = await Promise.all(
+    calls.map(async ({ sid, number }) => {
+      const e164number = parseNumber(number)
+      return {
+        sid,
+        // hash the number cos I don't want to store personal information geez
+        phoneHash: await bcrypt.hash(e164number, PHONE_BCRYPT_COST),
+        response: Responses.NO_RESPONSE_YET
+      }
+    })
+  )
+
+  return this.create(callsToCreate)
+}
+
+const Invitation = mongoose.model('Invitation', invitationSchema)
+
+module.exports = {
+  Invitation
+}
