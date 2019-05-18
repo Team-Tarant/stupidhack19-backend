449a4b01bbd2e02efe6ee38210291403f2edd95b
Hugo Holmqvist <hugo.holm@live.com>
2019-05-18 13:51:23 +0300

    Add neural network feedthrough

--- src/services/contentService.js
+++ src/services/contentService.js
@@ -1,12 +1,20 @@
 const axios = require('axios')
 
-const { CONTENT_SERVICE_URL } = process.env
+const {
+  CONTENT_SERVICE_URL,
+  NEURALNET_SERVICE_URL
+} = process.env
+
+const feedToNeuralNetwork = outputText => axios
+  .post(NEURALNET_SERVICE_URL, outputText)
+  .then(({ data }) => data.trim())
 
 const getContent = () =>
   axios
     .get(CONTENT_SERVICE_URL)
     .then(({ data }) => data)
     .then(({ text }) => text)
+    .then(feedToNeuralNetwork)
     .catch(e => {
       console.error('contentService::getContent failed')
       console.error(e)
